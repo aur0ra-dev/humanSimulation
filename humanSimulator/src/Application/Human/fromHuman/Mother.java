1dbc841588d07094835f1b03c1630b7001a4966f
JAkub <generovic@gmail.com>
2022-05-28 15:58:56 +0200

    mała zmiana

--- humanSimulator/src/Application/Human/fromHuman/Mother.java
+++ humanSimulator/src/Application/Human/fromHuman/Mother.java
@@ -11,4 +11,5 @@ public class Mother extends Human {
 
     }
 
+
 }
--- humanSimulator/src/Application/Human/fromHuman/Teacher.java
+++ humanSimulator/src/Application/Human/fromHuman/Teacher.java
@@ -3,4 +3,9 @@ package Application.Human.fromHuman;
 import Application.Human.Human;
 
 public class Teacher extends Human {
+    protected int abillityToTeach;
+    protected int yearsOfExperience;
+    protected void shareWisdom(){
+
+    }
 }
