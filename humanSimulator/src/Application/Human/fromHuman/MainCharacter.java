004f0c23cec15972628df27f54a318791f64e053
Xeviz <kubas2467@gmail.com>
2022-05-28 16:01:19 +0200

    wszyscy ludzie

--- humanSimulator/src/Application/Human/fromHuman/MainCharacter.java
+++ humanSimulator/src/Application/Human/fromHuman/MainCharacter.java
@@ -3,4 +3,22 @@ package Application.Human.fromHuman;
 import Application.Human.Human;
 
 public class MainCharacter extends Human {
+
+    int numberOfChildren = 0;
+    boolean haveAPartner = false;
+    int numberofFriends = 0;
+    int health = random.nextInt(80)+21;
+    int bonusesToEarnings = 0;
+
+    protected void gatherFromFriends () {
+
+    }
+    protected void gatherFromParents () {
+
+    }
+    protected void Reflections () {
+
+    }
+
+
 }
--- humanSimulator/src/Application/Human/fromHuman/Teacher.java
+++ humanSimulator/src/Application/Human/fromHuman/Teacher.java
@@ -3,4 +3,6 @@ package Application.Human.fromHuman;
 import Application.Human.Human;
 
 public class Teacher extends Human {
+    int abilityToTeach = random.nextInt(90)+11;
+    int yearOfExperience = 0;
 }
