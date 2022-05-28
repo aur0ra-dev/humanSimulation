e86a137ac1b8e7aeceec562381b557b4ef317c72
Xeviz <kubas2467@gmail.com>
2022-05-28 16:04:57 +0200

    shareWisdom

--- humanSimulator/src/Application/Human/fromHuman/Teacher.java
+++ humanSimulator/src/Application/Human/fromHuman/Teacher.java
@@ -5,4 +5,8 @@ import Application.Human.Human;
 public class Teacher extends Human {
     int abilityToTeach = random.nextInt(90)+11;
     int yearOfExperience = 0;
+
+    protected void shareWisdom() {
+
+    }
 }
