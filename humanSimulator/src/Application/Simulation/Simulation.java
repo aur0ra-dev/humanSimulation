4e6ca218a39975d20a86e879be170f6d6c14e13b
JAkub <generovic@gmail.com>
2022-05-29 17:46:22 +0200

    Application Simulation change

---
+++ humanSimulator/src/Application/Simulation/Simulation.java
@@ -0,0 +1,10 @@
+package Application.Simulation;
+
+import Application.Human.fromHuman.MainCharacter;
+
+public class Simulation {
+    public int duration;
+    MainCharacter mainCharacter = new MainCharacter();
+
+
+}
